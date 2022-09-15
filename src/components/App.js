6e24cc1efff96b9c4467e04f01806e9861e40c40
Pedro Pereira <plp.leoncio@gmail.com>
2022-09-14 22:34:59 -0300

    menu estilizado e parte do funcional

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import GlobalStyle from "../common/GlobalStyles";
 import Login from "./Login/Login";
+import Menu from "./Menu/Menu";
 
 export default function App() {
+
+  const [menuIsVisible, setMenuIsVisible] = useState(true)
+
   return (
     <>
       <GlobalStyle />
       <BrowserRouter>
+
+      <Menu menuIsVisible={menuIsVisible} setMenuIsVisible={setMenuIsVisible}/>
+
         <Routes>
+
           <Route path="/" element={<div>HELLO WORD</div>} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<div>cadastro</div>} />
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,146 @@
+import { useNavigate } from 'react-router-dom';
+import styled from "styled-components";
+import logo from "../../assets/img/logomarca.png"
+import user from "../../assets/img/user-icon.svg"
+import search from "../../assets/img/search-icon.svg"
+
+import { useState } from "react";
+import axios from "axios";
+
+
+export default function Menu ({menuIsVisible, setMenuIsVisible}) {
+    const [form, setForm] = useState({
+        search:""
+    })
+
+    const navigate = useNavigate();
+    function goToUser (){
+        setMenuIsVisible(!menuIsVisible)
+        navigate("/login")
+    }
+    function handleForm(e){
+        setForm({[e.target.name]: e.target.value})
+    }
+    async function sendForm(){
+        
+        try {
+            const res = await axios.get(`http://localhost:5000/search/${form.search}`) 
+            console.log(res)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        menuIsVisible ? (
+
+        <Container>
+
+            <MenuContainer>
+
+                <Logo>
+                    <img src={logo} onClick={() => navigate("/home")}/>                    
+                </Logo>
+
+                <Tittle onClick={() => navigate("/")}>MyPets</Tittle>
+
+                <User>
+                    <img src={user} onClick={() => {goToUser()}}/>
+                </User>
+
+            </MenuContainer>
+
+            <ContainerSearchBar>
+                <input placeholder="Buscar" name="search" onChange={handleForm} value={form.search} required></input>
+                <img src={search} onClick={() => {sendForm()}}/>
+            </ContainerSearchBar>
+            
+
+            
+        </Container>
+
+        ):(<></>)
+    )
+
+}
+const Container = styled.div`
+
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction:column;
+
+    width: 100%;
+
+    color:red;
+
+`
+const MenuContainer = styled.div`
+    margin-top: 15px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+
+    width: 100%;
+    height: 7vh;
+`
+const Logo = styled.div`
+    width:15%;
+
+    display:flex;
+    align-items:center;
+    justify-content:center;
+
+    img {
+        width: 75%;
+        height: 75%;
+    }
+`
+const Tittle = styled.div`
+    width:60%;
+
+    display:flex;
+    align-items:center;
+    justify-content:left;
+    
+    font-weight:600;
+    font-size: 20px;
+    color: #34495E;
+`
+const User = styled.div`
+    width:15%;
+
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    img {
+        width: 65%;
+        height: 65%;
+    }
+`
+const ContainerSearchBar = styled.form`
+    margin-top:15px;
+    margin-bottom: 35px;
+    width: 90%;
+    height: 5vh;
+    position: relative;
+    
+    input {
+        width:100%;
+        height:100%;
+
+        background: #FFFFFF;
+        box-shadow: 0px 4px 10px 2px rgba(0, 0, 0, 0.25);
+        border-radius: 30px;
+        border: none;
+        outline: none;
+        padding-left: 50px;
+        
+    }
+    img {
+        position: absolute;
+        right: 20px;
+        bottom: 10px;
+    }
+`
